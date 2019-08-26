0ebf6a70049a491d32fa9b6b352b27ae7caae62c
Thiago Augusto <44952468+thiagoslima19@users.noreply.github.com>
2019-08-26 00:07:41 -0300

    Add files via upload

---
+++ ThiagoAugustoLista3.R
@@ -0,0 +1,72 @@
+#Lista 3 - Program Estat 
+#1)
+#a)
+menor<-function(a,b){
+  if(a<b){
+    return(a)
+  }
+  return(b)
+}
+#b)
+menor_3<-function(a,b,c){
+  if(a<b && a<c){
+    return(a)
+  }else{
+    if(b<c)
+      return(b)
+  }
+  return(c)
+  
+}
+#2
+triangulo<-function(a,b,c){
+  if(a<=0 || b<=0 || c<=0){
+    stop("Digite medidas positivas")
+  }
+  if(a>b+c || b>a+c || c>a+b){
+    stop("Os dados digitados não formam um triângulo")
+  }
+  if(a==b && b==c){
+    cat("Equilátero")
+  }else{
+    if(a!=b && a!=c && b!=c){
+      cat("Escaleno")
+    }else{
+      cat("Isósceles")
+    }
+  }
+}
+#3
+Cont<-function(vet){
+  n=length(vet)
+  c=0
+  for(i in 1:n){
+    if(vet[i]>=0){
+      c=c+1
+    }
+  }
+  return(c)
+  }
+    
+v = c(1.0,3.2,???2.1,10.6,0.0,???1.7,???0.5)
+Cont(v)
+
+menoresq<-function(a,vet){
+  n=length(vet)
+  c=0
+  for(i in 1:n){
+    if(vet[i]<a)
+      c=c+1
+  }
+  return(c)
+}
+  `
+#4)
+nmultiplosdem<-function(n,m){
+  vet=NULL
+  for(i in 1:n){
+    vet[i]=m*i
+  }
+  return(vet)
+}
+
